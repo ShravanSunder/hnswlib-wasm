b14f89b958da4434a3be9aaf8e2d6bd44e75aa72
Shravan Sunder <ShravanSunder@users.noreply.github.com>
2023-04-06 21:37:02 -0400

    fixes gho-16.  added benchmarks

--- test/HierarchicalNSW.bench.ts
+++ test/HierarchicalNSW.bench.ts
@@ -1,17 +1,85 @@
 
 import { bench } from "vitest";
+import hnswlib from "~lib/hnswlib.mjs";
+import { adaDimensions, createVectorData } from "~test/testHelpers";
 import { defaultParams, HierarchicalNSW, HnswlibModule,  loadHnswlib,  } from "../dist/hnswlib";
 
-let hnswlib: HnswlibModule;
-let index: HierarchicalNSW;
 
-// const baseIndexSize = 1000;
-// bench(`benchmark initindex ${baseIndexSize * 10} points`, () => {
-//   const newIndexSize = baseIndexSize * 10;
-//   index.initIndex(newIndexSize, ...defaultParams.initIndex);
-// }, {
-//   setup: async () => {
-//     hnswlib = await loadHnswlib(),
-//     index = new hnswlib.HierarchicalNSW("l2", 3)
-//   }
-// });
+
+describe("benchmark initIndex", () => {
+  let hnswlib: HnswlibModule
+  let index: HierarchicalNSW;
+  const setup = async () => { 
+    // if (!hnswlib) {
+    hnswlib = await loadHnswlib();
+    index = new hnswlib.HierarchicalNSW("l2", adaDimensions);
+    // }
+  }
+  const baseIndexSize = 1000;
+  bench(`${baseIndexSize} points`, async () => {
+    const newIndexSize = baseIndexSize;
+    index.initIndex(newIndexSize, ...defaultParams.initIndex);
+  }, {
+    setup
+  }
+  );
+
+  bench(`${baseIndexSize * 10} points`, async () => {
+    const newIndexSize = baseIndexSize * 10;
+    index.initIndex(newIndexSize, ...defaultParams.initIndex);
+  }, {
+    setup
+  });
+
+  bench(`${baseIndexSize * 100} points`, async () => {
+    const newIndexSize = baseIndexSize * 100;
+    index.initIndex(newIndexSize, ...defaultParams.initIndex);
+  }, {
+    setup
+  });
+});
+
+
+
+describe("benchmark initIndex and addPoints", () => {
+  let hnswlib: HnswlibModule
+  let index: HierarchicalNSW;
+  const setup = async () => { 
+    // if (!hnswlib) {
+    hnswlib = await loadHnswlib();
+    index = new hnswlib.HierarchicalNSW("l2", adaDimensions);
+    // }
+  }
+  const baseIndexSize = 10;
+  bench(`${baseIndexSize} points`, async () => {
+    const newIndexSize = baseIndexSize;
+    index.initIndex(newIndexSize, ...defaultParams.initIndex);
+    const testVectorData = createVectorData(newIndexSize, adaDimensions);
+    
+    index.addItems(testVectorData.vectors, testVectorData.labels, ...defaultParams.addPoint);
+  }, {
+    setup,
+    iterations: 5
+  });
+
+  bench(`${baseIndexSize * 10} points`, async () => {
+    const newIndexSize = baseIndexSize * 10;
+    index.initIndex(newIndexSize, ...defaultParams.initIndex);
+    const testVectorData = createVectorData(newIndexSize, adaDimensions);
+    index.addItems(testVectorData.vectors, testVectorData.labels, ...defaultParams.addPoint);
+  }, {
+    setup,
+    iterations: 5
+  });
+
+  bench(`${baseIndexSize * 100} points`, async () => {
+    const newIndexSize = baseIndexSize * 100;
+    index.initIndex(newIndexSize, ...defaultParams.initIndex);
+    const testVectorData = createVectorData(newIndexSize, adaDimensions);
+    index.addItems(testVectorData.vectors, testVectorData.labels, ...defaultParams.addPoint);
+  }, {
+    setup,
+    iterations: 5
+  });
+
+});
